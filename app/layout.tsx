329aa038fde20b35ffec07f80210f70419ab2a7f
Kevin Mendez <mediaman502@aol.com>
2024-01-28 16:19:24 -0500

    adding webhooks

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
-//1:20:46
 
 const inter = Inter({ subsets: ['latin'] })
 
